335165fdf21456f28089fdc3e57e01f119244ca7
CarlosPadilla1429 <48220203+CarlosPadilla1429@users.noreply.github.com>
2019-03-04 12:25:05 -0600

    Add files via upload

---
+++ GitHub_GreenTea.cpp
@@ -0,0 +1,15 @@
+/*
+- GitHub_GreenTea_CarlosPadilla 	
+- License: GNU Public Updated: 3/4/19	
+- Author: Carlos Padilla
+- A starter basic C++ Program
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout <<"Hello GreenTea" << endl;
+	
+	return 0;	
+}
